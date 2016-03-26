6b30d195639e33aabe11fb5b194cdfd59140689c
isko-algorithm <gjpgagno@gmail.com>
2016-03-27 03:05:10 +0800

    created basic migration for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('employeeNumber')->unique();
+            $table->string('firstName');
+            $table->string('middleName');
+            $table->string('lastName');
+            $table->string('sex');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->integer('department');
+            $table->integer('position');
+            $table->integer('paygrade');
+            $table->decimal('salary',10,2);
+            $table->date('dateStarted');
+            $table->date('dateLeft');
+            $table->boolean('isManager')->default(0);
+            $table->boolean('isActive')->default(1);
             $table->timestamps();
         });
     }
