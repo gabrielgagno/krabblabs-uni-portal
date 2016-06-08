63ec3ba6a4683f2941da7c4447314a1354403b0d
isko-algorithm <gjpgagno@gmail.com>
2016-06-08 23:42:55 +0800

    added leave balance model

--- app/Models/User.php
+++ app/Models/User.php
@@ -84,4 +84,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('Benefit', 'user');
     }
+
+    public function leaveBalances()
+    {
+        return $this->hasMany('LeaveBalance', 'user');
+    }
 }
