269005834f0762ecb8ad089c7d3b2ba64dc7504a
gjpgagno <gjpgagno@gmail.com>
2016-09-16 10:57:23 +0800

    fixed ui for timesheet tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
                         @else
                             <li><a href="{{url('/')}}">Dashboard</a></li>
                             <li class="dropdown">
-                                <a href="{{url('/timesheet')}}" role="button" aria-expanded="false">Timesheet <span class="caret"></span></a>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Timesheet <span class="caret"></span></a>
                                 <ul class="dropdown-menu" role="menu">
                                     <li><a href="{{url('/timesheet')}}">Attendance</a></li>
                                     <li><a href="{{url('/timesheet/requests')}}">Requests</a></li>
