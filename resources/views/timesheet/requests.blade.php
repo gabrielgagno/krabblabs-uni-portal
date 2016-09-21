dd2fbf51913c1ce0e51e914cfe756b54cc091456
isko-algorithm <gjpgagno@gmail.com>
2016-09-22 00:20:48 +0800

    improved requests page

--- resources/views/timesheet/requests.blade.php
+++ resources/views/timesheet/requests.blade.php
@@ -5,15 +5,28 @@
     <div class="container">
         <h1 class="h1 page-header">Requests</h1>
         <div class="row">
-            <ul class="nav nav-tabs">
-                <li class="active"><a href="#leave" data-toggle="tab">Leave</a></li>
-                <li><a href="#timecorrection" data-toggle="tab">Time Correction</a></li>
-                <li><a href="#overtime" data-toggle="tab">Overtime</a></li>
-            </ul>
-            <div class="tab-content" id="tabs">
-                <div class="tab-pane fade active in" id="leave">...Content...</div>
-                <div class="tab-pane" id="timecorrection">...Content...</div>
-                <div class="tab-pane" id="overtime">...Content...</div>
+            <div class="col-md-12">
+                <ul class="nav nav-tabs nav-justified">
+                    <li class="active"><a href="#leave" data-toggle="tab">Leave</a></li>
+                    <li><a href="#timecorrection" data-toggle="tab">Time Correction</a></li>
+                    <li><a href="#overtime" data-toggle="tab">Overtime</a></li>
+                </ul>
+                <div class="tab-content" id="tabs">
+                    <div class="tab-pane fade active in" id="leave">
+                        <form method="post" action="{{url('/requests/leaves/')}}">
+                            {{csrf_field()}}
+                            <div class="form-group">
+                                
+                            </div>
+                        </form>
+                    </div>
+                    <div class="tab-pane" id="timecorrection">
+
+                    </div>
+                    <div class="tab-pane" id="overtime">
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
