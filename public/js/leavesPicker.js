2eda4af529f182763ffbd93942223a61f491770a
gjpgagno <gjpgagno@gmail.com>
2016-09-29 18:44:06 +0800

    added dayClick function in leave calendar picker

--- public/js/leavesPicker.js
+++ public/js/leavesPicker.js
@@ -4,6 +4,10 @@
 
 $(document).ready(function () {
     $('#applyLeaveCalendarModal').on('shown.bs.modal', function () {
-        $('#applyLeaveCalendar').fullCalendar({})
+        $('#applyLeaveCalendar').fullCalendar({
+            dayClick: function() {
+                alert('a day has been clicked!');
+            }
+        });
     });
 });
